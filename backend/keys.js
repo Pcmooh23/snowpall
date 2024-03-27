748710b5ff6a5a07a3f1552e02b58b06836dc773
Mooh23 <paulmooh@gmail.com>
2024-03-26 23:43:41 -0400

    keys

--- backend/keys.js
+++
@@ -1,7 +0,0 @@
-const path = require('path');
-require('dotenv').config({ path: path.resolve(__dirname, '.env') });
-
-module.exports = {
-    mongoURI: `mongodb+srv://paulmooh:${process.env.MONGO_PASSWORD}@cluster0.1esujv8.mongodb.net/`,
-    secretOrKey: "secret",
-};
