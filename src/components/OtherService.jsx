7f1d17c8452b5a965235152ff6a3043efbff9c00
Mooh23 <paulmooh@gmail.com>
2024-04-22 06:09:46 -0400

    no images

--- src/components/OtherService.jsx
+++ src/components/OtherService.jsx
@@ -220,28 +220,7 @@ const OtherService = () => {
                   </label>
                 </div>
               </div> 
-              <div className='other-upload-area'>
-                {otherPreview ? (
-                  <img 
-                  src={otherPreview} 
-                  alt="Preview" 
-                  className="uploaded-image-preview"
-                  style={{ maxWidth: '100px', cursor: 'pointer' }}
-                  onClick={handleIconClick}/>) : (
-                  <div htmlFor='image-upload' className='other-upload-label' onClick={handleIconClick}>
-                  <ImageIcon size={90} className='imageIcon'/>
-                  <p className='words'>Optional: Upload Image</p>
-                  </div> )}
-                {<input 
-                  id='image-upload'
-                  type='file' 
-                  name='image'
-                  className='image-upload' 
-                  accept="image/*" 
-                  ref={fileInputRef}
-                  onChange={handleImageChange}
-                  style={{ display: 'none' }}/>} 
-              </div>
+             
             </div>
               <textarea 
                 rows='5' 
